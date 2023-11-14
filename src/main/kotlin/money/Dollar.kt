82684e9d2790443823c98a72f9b6b2c292c6cf3a
blue32a <blue32a.jp@gmail.com>
2023-11-14 21:53:33 +0900

    trueを返すだけの仮実装を行う ー 第３章　三角測量

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -4,4 +4,7 @@ class Dollar(var amount: Int) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
+    override fun equals(other: Any?): Boolean {
+        return true
+    }
 }
