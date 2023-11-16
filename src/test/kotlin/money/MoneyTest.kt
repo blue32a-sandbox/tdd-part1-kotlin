ce86ad8c2afab7a73277c40f58a4ad027acb3cd3
blue32a <blue32a.jp@gmail.com>
2023-11-16 21:08:22 +0900

    Dollarを返すことを伝えるため、Dollar同士を直接比較する ー 第４章　意図を語るテスト

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -10,9 +10,9 @@ class MoneyTest {
     fun testMultiplication() {
         val five = Dollar(5)
         var product = five.times(2)
-        assertEquals(10, product.amount)
+        assertEquals(Dollar(10), product)
         product = five.times(3)
-        assertEquals(15, product.amount)
+        assertEquals(Dollar(15), product)
     }
     @Test
     fun testEquality() {
