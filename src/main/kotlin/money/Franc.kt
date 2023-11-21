c72ba6694ed09fcdd832051a104979468df04b13
blue32a <blue32a.jp@gmail.com>
2023-11-21 11:13:14 +0900

    完全に一致したFrancのequalsメソッドを削除する ー 第６章　テスト不足に気づいたら

--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -4,8 +4,4 @@ class Franc(amount: Int): Money(amount) {
     fun times(multiplier: Int): Franc {
         return Franc(amount * multiplier)
     }
-    override fun equals(other: Any?): Boolean {
-        val money = other as Money
-        return amount == money.xxxAmount()
-    }
 }
