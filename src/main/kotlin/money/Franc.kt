880dc46d519654298b53d3bddefd54d2493468f0
blue32a <blue32a.jp@gmail.com>
2023-11-21 11:11:46 +0900

    キャスト対象の型をMoneyに変更する（Franc） ー 第６章　テスト不足に気づいたら

--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -5,7 +5,7 @@ class Franc(amount: Int): Money(amount) {
         return Franc(amount * multiplier)
     }
     override fun equals(other: Any?): Boolean {
-        val franc: Money = other as Franc
+        val franc = other as Money
         return amount == franc.xxxAmount()
     }
 }
