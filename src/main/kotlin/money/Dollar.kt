05f1f742010683b3d4689e06c055b897ecfd1cb6
blue32a <blue32a.jp@gmail.com>
2023-11-21 11:02:02 +0900

    equalsメソッドをMoneyクラスに引き上げる ー 第６章　テスト不足に気づいたら

--- src/main/kotlin/money/Dollar.kt
+++ src/main/kotlin/money/Dollar.kt
@@ -4,8 +4,4 @@ class Dollar(amount: Int): Money(amount) {
     fun times(multiplier: Int): Dollar {
         return Dollar(amount * multiplier)
     }
-    override fun equals(other: Any?): Boolean {
-        val money = other as Money
-        return amount == money.xxxAmount()
-    }
 }
--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -1,6 +1,10 @@
 package money
 
 open class Money(protected val amount: Int) {
+    override fun equals(other: Any?): Boolean {
+        val money = other as Money
+        return amount == money.xxxAmount()
+    }
     fun xxxAmount(): Int {
         return amount
     }
