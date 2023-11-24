52643bb341f57d283d7630b759319a1c79ce7cb7
blue32a <blue32a.jp@gmail.com>
2023-11-24 21:19:50 +0900

    まずまずのやり方でテストを通す ー 第７章　疑念をテストに翻訳する

--- src/main/kotlin/money/Money.kt
+++ src/main/kotlin/money/Money.kt
@@ -4,5 +4,6 @@ open class Money(protected val amount: Int) {
     override fun equals(other: Any?): Boolean {
         val money = other as Money
         return amount == money.amount
+                && javaClass.name.equals(money.javaClass.name)
     }
 }
