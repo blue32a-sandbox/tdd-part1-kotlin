737176ec3257cd8eefd182aaae143926fc9a639f
blue32a <blue32a.jp@gmail.com>
2023-11-26 21:24:14 +0900

    Dollarを返すFactory Methodをテストで使う ー 第８章　実装を隠す

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -8,7 +8,7 @@ import kotlin.test.assertTrue
 class MoneyTest {
     @Test
     fun testMultiplication() {
-        val five = Dollar(5)
+        val five = Money.dollar(5)
         assertEquals(Dollar(10), five.times(2))
         assertEquals(Dollar(15), five.times(3))
     }
