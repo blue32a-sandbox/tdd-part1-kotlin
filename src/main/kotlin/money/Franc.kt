54c32b73e1ed1033556081b056951b3cdfff458d
blue32a <blue32a.jp@gmail.com>
2023-12-14 18:16:12 +0900

    ２つのサブクラスでそれぞれcurrencyメソッドを実装する ー 第９章　歩幅の調整

--- src/main/kotlin/money/Franc.kt
+++ src/main/kotlin/money/Franc.kt
@@ -1,6 +1,9 @@
 package money
 
 class Franc(amount: Int): Money(amount) {
+    override fun currency(): String {
+        return "CHF"
+    }
     override fun times(multiplier: Int): Money {
         return Franc(amount * multiplier)
     }
