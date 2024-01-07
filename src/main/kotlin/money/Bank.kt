5736031b573d0cf6c031e00777a93d9256544d36
blue32a <blue32a.jp@gmail.com>
2024-01-07 19:07:31 +0900

    キャストやクラスのチェックを排除する ー 第１３章　実装を導くテスト

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -2,9 +2,6 @@ package money
 
 class Bank {
     fun reduce(source: Expression, to: String): Money {
-        if (source is Money)
-            return (source as Money).reduce(to)
-        val sum = source as Sum
-        return sum.reduce(to)
+        return source.reduce(to)
     }
 }
