ec73cd52ef93ac66fb5e7e2534f283aab58100e9
blue32a <blue32a.jp@gmail.com>
2024-01-07 17:23:26 +0900

    式と銀行のメタファーを使ってテストを書き直す ー 第１２章　設計とメタファー

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -25,7 +25,10 @@ class MoneyTest {
     }
     @Test
     fun testSimpleAddition() {
-        val sum = Money.dollar(5).plus(Money.dollar(5))
-        assertEquals(Money.dollar(10), sum)
+        val five = Money.dollar(5)
+        val sum = five.plus(five)
+        val bank = Bank()
+        val reduced = bank.reduce(sum, "USD")
+        assertEquals(Money.dollar(10), reduced)
     }
 }
