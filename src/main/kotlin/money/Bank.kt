7aec3c0d2e39679b18de28fe65ed6001a279709e
blue32a <blue32a.jp@gmail.com>
2024-01-07 19:01:03 +0900

    reduceメソッドに渡されたのがMoneyの場合について実装 ー 第１３章　実装を導くテスト

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -2,6 +2,8 @@ package money
 
 class Bank {
     fun reduce(source: Expression, to: String): Money {
+        if (source is Money)
+            return source
         val sum = source as Sum
         return sum.reduce(to)
     }
