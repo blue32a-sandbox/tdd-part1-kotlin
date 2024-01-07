ee157a8a12be29e9a13a48a7df27a2bcfa20b8ab
blue32a <blue32a.jp@gmail.com>
2024-01-07 18:57:07 +0900

    Sumクラスへのフィールドへのアクセスを排除する ー 第１３章　実装を導くテスト

--- src/main/kotlin/money/Sum.kt
+++ src/main/kotlin/money/Sum.kt
@@ -1,4 +1,8 @@
 package money
 
 class Sum(public val augend: Money, public val addend: Money): Expression {
+    fun reduce(to: String): Money {
+        val amount = augend.amount + addend.amount
+        return Money(amount, to)
+    }
 }
