af018184af24114888fda8a84d18d44b01c199f2
blue32a <blue32a.jp@gmail.com>
2024-01-20 22:16:15 +0900

    為替レートを格納する ー 第１４章　学習用テストと回帰テスト

--- src/main/kotlin/money/Bank.kt
+++ src/main/kotlin/money/Bank.kt
@@ -1,12 +1,14 @@
 package money
 
 class Bank {
+    private val rates: MutableMap<Pair, Int> = HashMap()
     fun reduce(source: Expression, to: String): Money {
         return source.reduce(this, to)
     }
     fun addRate(from: String, to: String, rate: Int) {
+        rates[Pair(from, to)] = rate
     }
     fun rate(from: String, to: String): Int {
-        return if (from.equals("CHF") && to.equals("USD")) 2 else 1
+        return rates[Pair(from, to)] ?: throw NoSuchElementException("There is no '$from' to '$to' rate.")
     }
 }
