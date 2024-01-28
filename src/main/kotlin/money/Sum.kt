5cfeb4dba92229961750d40067fd6455f71ae772
blue32a <blue32a.jp@gmail.com>
2024-01-28 20:42:58 +0900

    Sumのplusメソッドを空実装 ー 第１５章　テスト任せとコンパイラ任せ

--- src/main/kotlin/money/Sum.kt
+++ src/main/kotlin/money/Sum.kt
@@ -1,6 +1,9 @@
 package money
 
 class Sum(val augend: Expression, val addend: Expression): Expression {
+    override fun plus(addend: Expression): Expression {
+        return Money(0, "USD")
+    }
     override fun reduce(bank: Bank, to: String): Money {
         val amount = augend.reduce(bank, to).amount +
                 addend.reduce(bank, to).amount
