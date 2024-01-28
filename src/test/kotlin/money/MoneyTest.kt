90521b8985aff53ce314e7eb7df0f08ad12a0e17
blue32a <blue32a.jp@gmail.com>
2024-01-28 20:38:01 +0900

    fiveBucks変数をExpressionに変更 ー 第１５章　テスト任せとコンパイラ任せ

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -65,7 +65,7 @@ class MoneyTest {
     }
     @Test
     fun testMixedAddition() {
-        val fiveBucks = Money.dollar(5)
+        val fiveBucks: Expression = Money.dollar(5)
         val tenFrancs: Expression = Money.franc(10)
         val bank = Bank()
         bank.addRate("CHF", "USD", 2)
